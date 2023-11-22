3ec127048360b504ed9f27a5e5e58cec9726ec43
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-11-22 14:35:28 +0400

    schemas are added

---
+++ src/schemas.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+import datetime as datetime
+from pydantic import BaseModel, Field
+
+
+class User(BaseModel):
+    id: int
+    username: str = Field(max_length=50)
+    password: str
+    is_admin: bool
+
+
+class Chat(BaseModel):
+    id: int
+    customer: Optional[User]
+    admin: Optional[User]
+
+
+class Message(BaseModel):
+    chat: Chat
+    datetime: datetime
+    text: str = Field(max_length=500)
