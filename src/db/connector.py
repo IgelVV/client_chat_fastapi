c5926b1fef691b4795083406a131eb0380a3c97e
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-11-23 15:28:27 +0400

    .env created

--- src/db/connector.py
+++ src/db/connector.py
@@ -1,13 +1,14 @@
 import pymysql.cursors
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
 def get_connection():
     return pymysql.connect(
-        host='db',
-        user='user',
-        password='password',
-        database='db',
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASS,
+        database=DB_NAME,
         charset='utf8mb4',
-        port=3306,
+        port=DB_PORT,
         cursorclass=pymysql.cursors.DictCursor,
     )
